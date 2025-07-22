9ded8b861a47a1120c93f671f8b0e18d11a24d11
NineSided <Ninesideddevelopment@gmail.com>
2025-07-22 17:31:27 +0100

    Build with gradle for the first time

---
+++ re-fine-1.21.8-0.0.0/src/client/java/com/ninesidedyt/re_fine/ReFineClient.java
@@ -0,0 +1,10 @@
+package com.ninesidedyt.re_fine;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ReFineClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
